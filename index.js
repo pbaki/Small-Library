9073001e10f1b8cf60c9112506e2827b14969867
pbaki <pbaki999@gmail.com>
2023-03-12 17:31:43 +0100

    Simple content display logic cleanup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let Book = class {
         this.read = read;
     }
     info() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} yet`;
+        return `Title is ${this.title}, written by ${this.author}, and have ${this.pages} number of pages`;
     }
 }
 
@@ -30,15 +30,27 @@ let Book = class {
 
     
 function addBookToLibrary(event){
-    const title1 = document.getElementById("title").value;
-    const author1 = document.getElementById("author").value;
-    const pages1 = document.getElementById("pages").value;
-    const read1 = document.getElementById("read").value;
+    let title1 = document.getElementById("title").value;
+    let author1 = document.getElementById("author").value;
+    let pages1 = document.getElementById("pages").value;
+    let read1 = document.getElementById("read").value;
 
-    //Initial validation and pushing object created by form to array
-    if (title1 == "" || author1 == "" || pages1 == "" || read1 == ""){
-        return "";
-    } else myLibrary.push(new Book(title1, author1, pages1, read1));
+    //Pushing object by form values to array
+
+    if (title1 == ""){
+        title1 = "Unknown";
+    }
+    if (author1 == "") {
+        author1 = "Unknown";
+    }
+    if (pages1 == "") {
+        pages1 = "Unknown";
+    }
+    if (read1 == "") {
+        read1 = "Unknown";
+    }
+
+    myLibrary.push(new Book(title1, author1, pages1, read1));
 
     //Getting card container and creating DOM for object
     const cardContainer = document.getElementById("card-container");
@@ -51,9 +63,9 @@ function addBookToLibrary(event){
     const authorp = document.createElement('p');
     authorp.innerHTML = "Author: " + author1;
     const pagesp = document.createElement('p');
-    pagesp.innerHTML = "Pages: " + pages1;
+    pagesp.innerHTML = "Number of pages: " + pages1;
     const readp = document.createElement('p');
-    readp.innerHTML = "Read: " + read1;
+    readp.innerHTML = "Read Status: " + read1;
     const infop = document.createElement('p');
     infop.innerHTML = "Info: " + myLibrary[myLibrary.length - 1].info();
 
